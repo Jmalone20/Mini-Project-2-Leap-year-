338ad050135b339936d5c53b871bafcc748d5264
Jmalone20 <jasmine.malone@patelhs.org>
2020-10-13 12:49:53 +0000

    Added print and if /else statements

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
-public class leapyear {
+public class Main {
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int year = 2020; 
+    Scanner scan = new Scanner(System.in);
+    System.out.println(year);
+    System.out.println("is it leap year");
+    boolean isLeap = scan.nextBoolean();
+       if((year % 4 == 0)&& (year % 100 == 0)) 
+       {
+        if(isLeap){ 
+          System.out.println("congrats it is leap year");
+        }
+       }
+      else{
+         System.out.println(" it is not leap year "); 
+      }
   }
 }
