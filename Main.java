33c55ce73a826128739e937614bd5b8e6390d7f2
Jmalone20 <jasmine.malone@patelhs.org>
2020-10-13 13:07:34 +0000

    I added a for loop letting the user guess 3 times

--- Main.java
+++ Main.java
@@ -5,7 +5,9 @@ public class Main {
     int year = 2020; 
     Scanner scan = new Scanner(System.in);
     System.out.println(year);
+    for(int i= 0; i < 3; i++){
     System.out.println("is it leap year");
+    }
     boolean isLeap = scan.nextBoolean();
        if((year % 4 == 0)&& (year % 100 == 0)) 
        {
